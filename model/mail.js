9b37a798ccbda9ce2a69f1033a3bb5ad4549f9be
fede maidan <fede.maidan@gmail.com>
2017-04-15 19:21:13 -0300

    corrigiendo

--- model/mail.js
+++ model/mail.js
@@ -1,6 +1,5 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-var bcrypt = require('bcrypt');
 // Thanks to http://blog.matoski.com/articles/jwt-express-node-mongoose/
  
 // set up a mongoose model
